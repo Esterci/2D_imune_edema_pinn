c4f77549cbd6b57144aa951eedf43d020dfa1f81
Esterci <thiago.esterci@engenharia.ufjf.br>
2024-05-20 16:18:49 -0300

    Organização para merge e estudo de granularidade

---
+++ edo/load_check_points.sh
@@ -0,0 +1,22 @@
+
+d=1716221142
+
+#################
+
+unzip -o check_points/jobs_$d.zip
+
+#################
+
+unzip -o check_points/error_files_$d.zip
+
+#################
+
+unzip -o check_points/output_files_$d.zip
+
+#################
+
+unzip -o check_points/edo_pinn_sim_$d.zip
+
+#################
+
+unzip -o check_points/learning_curves_$d.zip
