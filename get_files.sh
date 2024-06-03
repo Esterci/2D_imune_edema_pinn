ce11e6980e714061dbb6c61ef598b23fe7723d25
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2024-06-03 11:55:53 -0300

    organização do repositório

---
+++ get_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+scp thiago.esterci@cluster.mmc.ufjf.br:/home/thiago.esterci/2D_imune_edema_pinn/jobs/* ./jobs
+
+scp thiago.esterci@cluster.mmc.ufjf.br:/home/thiago.esterci/2D_imune_edema_pinn/edo_pinn_sim/* ./edo_pinn_sim
+
+scp thiago.esterci@cluster.mmc.ufjf.br:/home/thiago.esterci/2D_imune_edema_pinn/error_files/* ./error_files
+
+scp thiago.esterci@cluster.mmc.ufjf.br:/home/thiago.esterci/2D_imune_edema_pinn/learning_curves/* ./learning_curves
+
