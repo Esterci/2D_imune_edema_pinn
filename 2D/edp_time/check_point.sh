ce42df086c1ff4e89fec0f96667b15c91fd9d170
Esterci <thiago.esterci@engenharia.ufjf.br>
2024-09-16 16:34:46 -0300

    Adicionando sincronização de bucket nos resultados

--- 2D/edp_time/check_point.sh
+++ 2D/edp_time/check_point.sh
@@ -4,28 +4,32 @@ d=$(date +%s)
 
 zip -r jobs_$d.zip jobs/ 
 
-mv jobs_$d.zip check_points/
+mv jobs_$d.zip $LOCAL_PINN_GIT
 
 #################
 
 zip -r error_files_$d.zip error_files/ 
 
-mv error_files_$d.zip check_points/
+mv error_files_$d.zip $LOCAL_PINN_GIT
 
 #################
 
 zip -r output_files_$d.zip output_files/ 
 
-mv output_files_$d.zip check_points/
+mv output_files_$d.zip $LOCAL_PINN_GIT
 
 #################
 
 zip -r edo_pinn_sim_$d.zip edo_pinn_sim/ 
 
-mv edo_pinn_sim_$d.zip check_points/
+mv edo_pinn_sim_$d.zip $LOCAL_PINN_GIT
 
 #################
 
 zip -r learning_curves_$d.zip learning_curves/ 
 
-mv learning_curves_$d.zip check_points/
+mv learning_curves_$d.zip $LOCAL_PINN_GIT
+
+# Sending files to bucket 
+
+mgc object-storage objects sync --local=$LOCAL_PINN_GIT --bucket=$PINN_BUCKET
--- 2D/edp_time/load_check_points.sh
+++ 2D/edp_time/load_check_points.sh
@@ -1,26 +1,29 @@
+# Geting files to bucket 
+
+mgc object-storage objects download-all --src=$PINN_BUCKET --dst=$LOCAL_PINN_GIT 
 
 d=1721243952
 
 #################
 
-unzip -o check_points/jobs_$d.zip
+unzip -o $LOCAL_PINN_GIT/jobs_$d.zip
 
 #################
 
-unzip -o check_points/error_files_$d.zip
+unzip -o $LOCAL_PINN_GIT/error_files_$d.zip
 
 #################
 
-unzip -o check_points/output_files_$d.zip
+unzip -o $LOCAL_PINN_GIT/output_files_$d.zip
 
 #################
 
-unzip -o check_points/edo_pinn_sim_$d.zip
+unzip -o $LOCAL_PINN_GIT/edo_pinn_sim_$d.zip
 
 #################
 
-unzip -o check_points/edo_fdm_sim_$d.zip
+unzip -o $LOCAL_PINN_GIT/edo_fdm_sim_$d.zip
 
 #################
 
-unzip -o check_points/learning_curves_$d.zip
+unzip -o $LOCAL_PINN_GIT/learning_curves_$d.zip
