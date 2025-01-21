59e80d39fead6826b377bd9e9e924413c9cd49f6
Esterci <thiago.esterci.fernandes@gmail.com>
2025-01-21 08:53:01 -0300

    Alocating gpu memmory

--- 2D/edp_time/run_fvm_jobs.py
+++ 2D/edp_time/run_fvm_jobs.py
@@ -4,4 +4,4 @@ count = 0
 
 for i in range(2):
 
-    os.system("time ~/.conda/envs/torch-numba/bin/python3 fvm_comparison.py")
+    os.system("time ~/anaconda3/envs/torch-numba/bin/python3 fvm_comparison.py")
