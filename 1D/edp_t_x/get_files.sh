b94c17f7a08233b1c9c2e4b7cc2bf716b9212aee
Esterci <thiago.esterci.fernandes@gmail.com>
2025-06-02 15:40:33 -0300

    wip(inference): building inference script

--- 1D/edp_t_x/get_files.sh
+++ 1D/edp_t_x/get_files.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/2D/edp_t_x_y/control_dicts/* ./control_dicts
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/2D/edp_t_x_y/error_files/* ./error_files
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/2D/edp_t_x_y/fvm_sim/* ./fvm_sim
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/2D/edp_t_x_y/jobs/* ./jobs
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/2D/edp_t_x_y/learning_curves/* ./learning_curves
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/2D/edp_t_x_y/nn_parameters/* ./nn_parameters
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/2D/edp_t_x_y/output_files/* ./output_files
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/2D/edp_t_x_y/pinn_sim/* ./pinn_sim
+scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/control_dicts/* ./control_dicts
+scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/error_files/* ./error_files
+scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/fvm_animations/* ./fvm_animations
+scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/fvm_sim/* ./fvm_sim
+scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/jobs/* ./jobs
+scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/learning_curves/* ./learning_curves
+scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/nn_parameters/* ./nn_parameters
+scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/output_files/* ./output_files
+scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/pinn_sim/* ./pinn_sim
