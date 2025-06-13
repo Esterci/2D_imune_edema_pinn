ad213a76e719d918b0734692f07a5686e6326197
Esterci <thiago.esterci.fernandes@gmail.com>
2025-06-13 10:29:11 -0300

    feat(analysis): new results

--- 1D/edp_t_x/shell/get_files.sh
+++ 1D/edp_t_x/shell/get_files.sh
@@ -1,13 +1,65 @@
 #!/bin/bash
 
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/control_dicts/* ./control_dicts
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/error_files/* ./error_files
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/fvm_animations/* ./fvm_animations
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/fvm_sim/* ./fvm_sim
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/jobs/* ./jobs
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/learning_curves/* ./learning_curves
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/nn_parameters/* ./nn_parameters
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/output_files/* ./output_files
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/pinn_sim/* ./pinn_sim
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/scale_weights/* ./scale_weights
-scp $CLUSTER_USER@$CLUSTER_DNS:/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x/source_points/* ./source_points
+# Function to display usage instructions
+usage() {
+    echo "Usage: $0 [fvm|pinn-training|pinn-inference|control|jobs|scale|source|all]"
+    exit 1
+}
+
+# Check if at least one argument is provided
+if [ $# -eq 0 ]; then
+    usage
+fi
+
+# Define the base path for remote files
+REMOTE_PATH="/home/thiago.esterci/2D_imune_edema_pinn/1D/edp_t_x"
+REMOTE="$CLUSTER_USER@$CLUSTER_DNS:$REMOTE_PATH"
+
+# Process the provided argument
+case "$1" in
+    fvm)
+        echo "Downloading FVM simulation files..."
+        scp "$REMOTE/fvm_animations/*" ./fvm_animations
+        scp "$REMOTE/fvm_sim/*" ./fvm_sim
+        ;;
+    pinn-training)
+        echo "Downloading PINN training files..."
+        scp "$REMOTE/learning_curves/*" ./learning_curves
+        scp "$REMOTE/nn_parameters/*" ./nn_parameters
+        ;;
+    pinn-inference)
+        echo "Downloading PINN inference files..."
+        scp "$REMOTE/pinn_sim/*" ./pinn_sim
+        ;;
+    control)
+        echo "Downloading control_dicts..."
+        scp "$REMOTE/control_dicts/*" ./control_dicts
+        scp "$REMOTE/scale_weights/*" ./scale_weights
+        scp "$REMOTE/source_points/*" ./source_points
+        ;;
+    jobs)
+        echo "Downloading job and output files..."
+        scp "$REMOTE/jobs/*" ./jobs
+        scp "$REMOTE/error_files/*" ./error_files
+        scp "$REMOTE/output_files/*" ./output_files
+        ;;
+    all)
+        echo "Downloading all project files..."
+        scp "$REMOTE/control_dicts/*" ./control_dicts
+        scp "$REMOTE/error_files/*" ./error_files
+        scp "$REMOTE/fvm_animations/*" ./fvm_animations
+        scp "$REMOTE/fvm_sim/*" ./fvm_sim
+        scp "$REMOTE/jobs/*" ./jobs
+        scp "$REMOTE/learning_curves/*" ./learning_curves
+        scp "$REMOTE/nn_parameters/*" ./nn_parameters
+        scp "$REMOTE/output_files/*" ./output_files
+        scp "$REMOTE/pinn_sim/*" ./pinn_sim
+        scp "$REMOTE/scale_weights/*" ./scale_weights
+        scp "$REMOTE/source_points/*" ./source_points
+        ;;
+    *)
+        usage
+        ;;
+esac
+
+echo "Download completed."
